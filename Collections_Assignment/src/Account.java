d4591e7544d32f96a5fd1e3e899dfccf15438443
Home <Home@192.168.44.13>
2021-02-15 18:40:32 -0500

    Collections

---
+++ Collections_Assignment/src/Account.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+	
+	private long id;
+	private String accountType;
+	
+	public Account(long id,  String accountType) {
+		super();
+		this.id = id;
+		this.accountType = accountType;
+	}
+	
+	public static List<Account> createAccounts (int numAccounts) {
+		List<Account> accounts = new ArrayList<>();
+		
+		while(numAccounts > 0) {
+			Account a = new Account(0 + numAccounts, "savings");
+			accounts.add(a);
+			numAccounts--;
+		}
+		
+		return accounts;
+	}
+	
+	@Override
+	public String toString() {
+		return "Account [id: "+ this.id +"]";
+	}
+}
