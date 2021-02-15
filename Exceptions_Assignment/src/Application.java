c27e32ad3c8a510dd8507e244414d01c44bd35ad
Home <Home@192.168.44.13>
2021-02-15 15:21:33 -0500

    Exceptions

---
+++ Exceptions_Assignment/src/Application.java
@@ -0,0 +1,13 @@
+
+public class Application {
+	public static void main(String[] args) {
+		
+		Runner runner = new Runner();
+		
+		try {
+			runner.run();
+		} catch (CustomException e) {
+			e.printStackTrace();
+		}
+	}
+}
