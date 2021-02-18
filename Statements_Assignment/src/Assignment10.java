31b33f4c1545cd9b93eb00096fe43867028f9505
HONG JIE ZHANG <70243495+hongjie94@users.noreply.github.com>
2021-02-17 19:02:07 -0500

    Statements
    Statements

--- Statements_Assignment/src/Assignment10.java
+++ Statements_Assignment/src/Assignment10.java
@@ -1,5 +1,6 @@
 
 public class Assignment10 {
+	
 	public static void main(String[] args) {
 		double[] myNum = {1.1, 1.2, 1.3, 1.4 ,1.5 ,1.6, 1.7, 1.8, 1.9, 2.0};
 		for(int i = 0; i < myNum.length; i++) {
